fe582430a29cd397eea94dd80f154e4ebcc067e2
Sonu Yadav <sy239370@gmail.com>
2025-10-09 13:37:19 +0530

    Movie App React Native initialised

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View className='flex items-center justify-center'>
+      <Text className=''>index</Text>
+    </View>
+  )
+}
+
+export default index
