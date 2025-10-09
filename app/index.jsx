8d4ccd6cbb1242bf9d30b1716fc723bdfbfef5a1
Sonu Yadav <sy239370@gmail.com>
2025-10-09 13:45:57 +0530

    Native wind setup completed

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from "react";
+import { Text, View } from "react-native";
 
 const index = () => {
   return (
-    <View className='flex items-center justify-center'>
-      <Text className=''>index</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="font-bold color-dark-200 text-4xl">Flash</Text>
     </View>
-  )
-}
+  );
+};
 
-export default index
+export default index;
