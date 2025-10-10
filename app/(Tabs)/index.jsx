e9d2c0924359a648387a8398c335c287587c7352
Sonu Yadav <sy239370@gmail.com>
2025-10-10 23:19:28 +0530

    Latest movies section completed

--- app/(Tabs)/index.jsx
+++ app/(Tabs)/index.jsx
@@ -5,7 +5,14 @@ import useFetch from "@/customHooks/useFetch";
 import { getMovies } from "@/Services/Operations/movieOperation";
 import { useRouter } from "expo-router";
 import React from "react";
-import { ActivityIndicator, Image, ScrollView, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  FlatList,
+  Image,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
 const index = () => {
   const router = useRouter();
   let {
@@ -33,7 +40,6 @@ const index = () => {
               size="large"
               color={"#a8b5db"}
               className="mb-14 self-center"
-              
             />
           </View>
         ) : moviesError ? (
@@ -48,7 +54,29 @@ const index = () => {
                 placeholder={"Search for a Movie"}
               />
             </View>
-            <Text className="text-lg text-white font-bold mt-5 mb-3">Latest Movies</Text>
+            <Text className="text-lg text-white font-bold mt-5 mb-3">
+              Latest Movies
+            </Text>
+            <FlatList
+              data={movies}
+              renderItem={({ item }) => (
+                <View className="bg-dark-100 px-2 rounded-full ">
+                  <Text className="text-white text-sm ">{item.title}</Text>
+                </View>
+              )}
+              keyExtractor={(item) => item.id.toString()}
+              numColumns={13}
+              columnWrapperStyle={{
+                justifyContent: "flex-start",
+                gap: 10,
+                paddingRight: 5,
+                marginBottom: 10,
+                flexWrap: "wrap",
+              }}
+              className="mt-2 pb-32 flex-1"
+              scrollEnabled={false}
+              nestedScrollEnabled={true}
+            />
           </View>
         )}
       </ScrollView>
