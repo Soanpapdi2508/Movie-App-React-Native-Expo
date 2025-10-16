9264848d04030d651a594ba5b277cf302690042a
Sonu Yadav <sy239370@gmail.com>
2025-10-16 21:48:46 +0530

    screenshot is now disabled

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,14 +1,20 @@
 import { Stack } from "expo-router";
+import * as ScreenCapture from "expo-screen-capture";
+import { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import "./global.css";
 
 export default function RootLayout() {
+  useEffect(() => {
+    ScreenCapture.preventScreenCaptureAsync();
+    return () => ScreenCapture.preventScreenCaptureAsync();
+  }, []);
   return (
     <SafeAreaProvider>
-        <Stack>
-          <Stack.Screen name="(Tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="/movie/[id]" options={{ headerShown: false }} />
-        </Stack>
+      <Stack>
+        <Stack.Screen name="(Tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="/movie/[id]" options={{ headerShown: false }} />
+      </Stack>
     </SafeAreaProvider>
   );
 }
