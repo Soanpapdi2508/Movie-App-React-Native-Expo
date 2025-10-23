c2ed8b17dad4b62fdb237e897e5d49eb7cc56b54
Sonu Yadav <sy239370@gmail.com>
2025-10-23 16:44:24 +0530

    added saved slices

---
+++ Store/Slices/Saved.js
@@ -0,0 +1,21 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+const savedInitialState = {
+  savedData: [],
+};
+
+const savedSlice = createSlice({
+  name: "saved",
+  initialState: savedInitialState,
+  reducers: {
+    setSavedData: (state, value) => {
+      state.savedData = value.payload;
+    },
+    addSaved: (state, value) => {
+      state.savedData.push(value.payload);
+    },
+  },
+});
+
+export const { setSavedData, addSaved } = savedSlice.actions;
+export default savedSlice.reducer;
